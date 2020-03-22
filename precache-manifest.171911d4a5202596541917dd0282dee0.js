3b38741e5a2206932a3de8c42391cd263228ef51
Michael Molina <michaelmolina679@gmail.com>
2020-03-21 20:59:38 -0600

    Updates

---
+++ precache-manifest.171911d4a5202596541917dd0282dee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eecc593f4799a2ca9eafd5e4a994c15",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "919da621fe83f872f951",
+    "url": "/my-blog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "470a775a2085b0df5079",
+    "url": "/my-blog/static/js/2.1e0b480d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-blog/static/js/2.1e0b480d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919da621fe83f872f951",
+    "url": "/my-blog/static/js/main.66ed9cd7.chunk.js"
+  },
+  {
+    "revision": "4daf870c97b444f1d6b1",
+    "url": "/my-blog/static/js/runtime-main.08d26d9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
